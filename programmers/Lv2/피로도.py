cc9138db06916b48fce4cf2356ce65b940234573
HyeonSeungYang <yanghs@bizdata.kr>
2021-10-26 09:06:26 +0900

    update

---
+++ programmers/Lv2/피로도.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+
+
+def solution(k, dungeons):
+    size = len(dungeons)
+    cases = list(permutations(range(size), size))
+
+    answer = []
+    for case in cases:
+        # 현재 피로도
+        energy = k
+        # 탐험 던전 수
+        count = 0
+        for c in case:
+            # 최소 필요 필요도, 소모 피로도
+            energy_limit, energy_usage = dungeons[c]
+            if energy >= energy_limit:
+                energy -= energy_usage
+                count += 1
+        answer.append(count)
+
+    return max(answer)
